f154a9509ca314164a27e95a855e1c9395cea6a1
Jim D <heyjim89@gmail.com>
2015-07-27 15:16:35 -0600

    Init

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var indexController = require('./controllers/index.js');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/characters')
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', indexController.index);
+
+var server = app.listen(3000, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
